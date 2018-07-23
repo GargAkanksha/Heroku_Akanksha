ec3ba456effb8fd7f05a15176adcf4bf7141e23a
GargAkanksha <akanksha.30@gmail.com>
2018-07-23 06:12:35 -0500

    Fifth Commit - Modified twitter_status.py

--- twitter_status.py
+++ twitter_status.py
@@ -6,17 +6,17 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
-quotes_list = ["This is tweet 1","This is tweet 2", "This is tweet 3"]
+quotes_list = ["This is tweet 11","This is tweet 12", "This is tweet 13"]
 
 def tweet_func(tweet_number):
     api.update_status(quotes_list[tweet_number])
     print(f' Tweet #{tweet_number} tweeted successfully')
 
-counter = 0
+counter = 1
 
 while(True):
     tweet_func(counter)
     time.sleep(10)
     counter+=1
-    if counter>2:
+    if counter>3:
         break
